375c49040a8c651f8438eb341c45dc65c6cb4c19
Venreaver <reaversindustry@gmail.com>
2018-12-04 02:02:32 +0300

    maven: third step

--- src/test/java/com/epam/javatraining/SumCalculatorTest.java
+++ src/test/java/com/epam/javatraining/SumCalculatorTest.java
@@ -3,11 +3,15 @@ package com.epam.javatraining;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 @Test
 public class SumCalculatorTest {
     public void testSum() {
         SumCalculator sumCalculator = new SumCalculator();
-        assertEquals(sumCalculator.sum(2, 8), 10);
+        int sum = sumCalculator.sum(2, 8);
+        assertEquals(sum, 10);
+        boolean flag = sum == 10;
+        assertTrue(flag);
     }
 }
