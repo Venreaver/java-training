3b08a85978b3bdd247d553daf12356da1d8ae8a4
Venreaver <reaversindustry@gmail.com>
2019-02-28 02:30:24 +0300

    transactions & AOP: step 4 - added abstract class

--- webappserver/src/main/java/com/epam/javatraining/dogapp/service/CglibTransactionalDogService.java
+++ webappserver/src/main/java/com/epam/javatraining/dogapp/service/CglibTransactionalDogService.java
@@ -1,37 +1,12 @@
 package com.epam.javatraining.dogapp.service;
 
 import com.epam.javatraining.dogapp.dao.JdbcConnectionHolder;
-import lombok.RequiredArgsConstructor;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.InvocationHandler;
 
-import java.lang.reflect.Method;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-@RequiredArgsConstructor
-public class CglibTransactionalDogService implements InvocationHandler {
-    private final JdbcConnectionHolder holder;
-    private final Object target;
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        try {
-            Connection connection = holder.getConnection();
-            connection.setAutoCommit(false);
-            try {
-                Object result = method.invoke(target, args);
-                connection.commit();
-                return result;
-            } catch (SQLException e) {
-                connection.rollback();
-                throw e;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            holder.closeConnection();
-        }
+public class CglibTransactionalDogService extends TransactionalService implements InvocationHandler {
+    public CglibTransactionalDogService(JdbcConnectionHolder holder, Object target) {
+        super(holder, target);
     }
 
     public static Object getInstance(Object target, JdbcConnectionHolder holder) {
--- webappserver/src/main/java/com/epam/javatraining/dogapp/service/TransactionalProxy.java
+++ webappserver/src/main/java/com/epam/javatraining/dogapp/service/TransactionalProxy.java
@@ -1,38 +1,14 @@
 package com.epam.javatraining.dogapp.service;
 
 import com.epam.javatraining.dogapp.dao.JdbcConnectionHolder;
-import lombok.RequiredArgsConstructor;
 
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 import static java.lang.reflect.Proxy.newProxyInstance;
 
-@RequiredArgsConstructor
-public class TransactionalProxy implements InvocationHandler {
-    private final JdbcConnectionHolder holder;
-    private final Object target;
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        try {
-            Connection connection = holder.getConnection();
-            connection.setAutoCommit(false);
-            try {
-                Object result = method.invoke(target, args);
-                connection.commit();
-                return result;
-            } catch (SQLException e) {
-                connection.rollback();
-                throw e;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            holder.closeConnection();
-        }
+public class TransactionalProxy extends TransactionalService implements InvocationHandler {
+    public TransactionalProxy(JdbcConnectionHolder holder, Object target) {
+        super(holder, target);
     }
 
     public static Object getInstance(Object target, JdbcConnectionHolder holder) {
