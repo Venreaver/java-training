f40794207540e19266df0887a6934d7fd9f9a7f9
Venreaver <reaversindustry@gmail.com>
2019-04-05 17:51:55 +0300

    puzzle-wG3JaxG4: we should flush changes to the DB & clear cache in session to get original object from DB

--- src/test/java/io/qala/javatraining/dao/HibernateDogDaoTest.java
+++ src/test/java/io/qala/javatraining/dao/HibernateDogDaoTest.java
@@ -2,31 +2,26 @@ package io.qala.javatraining.dao;
 
 import io.qala.javatraining.HibernateDaoTest;
 import io.qala.javatraining.domain.Dog;
-import io.qala.javatraining.domain.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
-import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.Collection;
-
-import static io.qala.datagen.RandomShortApi.unicode;
-import static io.qala.datagen.RandomValue.length;
-import static io.qala.datagen.StringModifier.Impls.suffix;
 import static io.qala.javatraining.TestUtils.assertReflectionEquals;
-import static org.testng.Assert.*;
 
-@Test @HibernateDaoTest
+@Test
+@HibernateDaoTest
 public class HibernateDogDaoTest extends AbstractTransactionalTestNGSpringContextTests {
-    /** Find the description of the puzzle in the root README.md */
+    /**
+     * Find the description of the puzzle in the root README.md
+     */
     public void getsTheSameDogAsWasSaved() {
         Dog original = Dog.random();
         dao.createDog(original);
+        dao.flushAndClear();
         Dog fromDb = dao.getDog(original.getId());
         assertReflectionEquals(original, fromDb);
     }
 
-    @Autowired private HibernateDogDao dao;
+    @Autowired
+    private HibernateDogDao dao;
 }
